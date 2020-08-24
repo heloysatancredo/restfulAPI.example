3546857360990d3731b192fd9aacaf5f38aecce7
Heloysa Tancredo <heloysatancredo@Heloysas-MacBook-Pro.local>
2020-08-24 18:41:08 -0300

    adjustments

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 //alert("Hello");
 
-fetch('https://restcountries.eu/rest/v2/all')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    data.forEach(function (each) {
-      console.log(each.name)
-      $('.sub-child2').append(`<img src="${each.flag}" height="42" width="42">`)
-      $('.sub-child3').append(each.name + '  ')
+let btn = document.getElementById('load').addEventListener('click', () => {
+  loadData()
+})
+
+let loadData = () => {
+  fetch('https://restcountries.eu/rest/v2/all')
+    .then((response) => {
+      return response.json()
     })
-    console.log(JSON.stringify(data))
-  })
-  .catch((error) => {
-    console.error(error.message)
-  })
+    .then((data) => {
+      data.forEach(function (each) {
+        console.log(each.name)
+        $('.sub-child2').append(
+          `<img src="${each.flag}" height="42" width="42"> ${each.name} | `
+        )
+      })
+      console.log(JSON.stringify(data))
+    })
+    .catch((error) => {
+      console.error(error.message)
+    })
+}
